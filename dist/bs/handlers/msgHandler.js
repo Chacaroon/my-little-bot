880620a53bdd1e0580abd88a6d140655969c1e00
Chacaoon <nekit2326@gmail.com>
2017-07-11 18:36:44 +0300

    Debug Facebook API request

--- dist/bs/handlers/msgHandler.js
+++ dist/bs/handlers/msgHandler.js
@@ -69,6 +69,8 @@ var MessageHandler = function () {
 
 			var senderId = void 0;
 
+			console.dir(payload.messagingEvents); //eslint-disable-line
+
 			payload.messagingEvents.map(function (event) {
 				senderId = event.sender.id;
 				var text = event.message.text.trim().substring(0, 200);
